e80b15cd88974cc4ba404dc8030e860d19209e25
Nisanth Selvaraj <121391945+Nisanth2004@users.noreply.github.com>
2024-03-24 20:34:26 +0530

    Spring Security Demo

---
+++ Security-Demo/src/main/java/com/nisanth/springboot/AppSecurityConfig.java
@@ -0,0 +1,11 @@
+//package com.nisanth.springboot;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class AppSecurityConfig extends
+//{
+//
+//}
